b72977f55360ab8e2bb7f59016a27d3c8fdfc17d
Laár Eszter <laar.eszter@gmail.com>
2021-06-23 11:49:34 +0200

    028 feladat

---
+++ selenium-homework/bartender.py
@@ -0,0 +1,15 @@
+x = int(input("Hány éves vagy?"))
+y = input("Írd be a kért italt: sör vagy kóla")
+
+if x < 18 and y = 'sör':
+    print("Túl fiatal vagy:(")
+elif x >= 18:
+    print("Parancsoljon, a söre")
+if x > 60 and y ="kóla":
+    print("A koffein megemelheti a vérnyomását.")
+elif x >= 60:
+    print("Parancsoljon, a kólája.")
+if y != "sör" and y != "kóla":
+    print("Csak sör és kóla van.")
+
+
