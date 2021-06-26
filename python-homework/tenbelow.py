bebc6254ba9f729d0647c536fd6b1792d512c97c
Laár Eszter <laar.eszter@gmail.com>
2021-06-26 10:38:25 +0200

    032 feladat

--- python-homework/tenbelow.py
+++ python-homework/tenbelow.py
@@ -1,9 +1,20 @@
-x = int(input())
+#szám beírását kérem a felhasználótól
+x = int(input("Írj be egy számot!"))
 
+#számmá alakítok
 my_input = input()
 x += int(my_input)
 
+#10-nél kisebb számokat kiírom
 while x < 10:
     print(x)
+
+#a beírt számokat összeadom
+#sum =
+
+#kiírom az összeget
+#print(sum)
+
+
 
 
