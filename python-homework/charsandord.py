8d1040714f16d1a54464b2f502a24af97953f815
Laár Eszter <laar.eszter@gmail.com>
2021-06-26 10:20:24 +0200

    003 feladat

--- python-homework/charsandord.py
+++ python-homework/charsandord.py
@@ -1,5 +1,7 @@
+#kódok és a hozzátartozó betűk kiiratása
 for i in range(26):
     szam = i + 97
     print(szam, chr(szam))
 
-
+#sorokba rendezés
+#print(szam, chr(szam,),szam, chr(szam,),szam, chr(szam,))
