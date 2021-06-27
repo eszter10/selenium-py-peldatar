8a0dbfa1a35e7b6b64a7df84f9718a81039d5366
Laár Eszter <laar.eszter@gmail.com>
2021-06-27 15:46:38 +0200

    010 feladat kész

--- python2-homework/fordito.py
+++ python2-homework/fordito.py
@@ -1,13 +1,17 @@
+#létrehozok egy listát
 my_list = []
 
+#felhasználó tól kérek számokat amíg nem ír 0-t, listába rendezem
 while True:
     my_x = int(input("Írj be egy számot!"))
     if my_x == 0:
         break
     my_list.append(my_x)
 
+#megfordítom a számok sorrendjét
 my_list2 = my_list[::-1]
 
+#kiiratom a fordított listát
 print(my_list2)
 
 
