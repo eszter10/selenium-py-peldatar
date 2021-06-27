a8d573cbb8f022238c730c8f21030985877e2375
Laár Eszter <laar.eszter@gmail.com>
2021-06-27 15:43:10 +0200

    010 feladat kész

--- python2-homework/fordito.py
+++ python2-homework/fordito.py
@@ -1,11 +1,15 @@
+my_list = []
+
 while True:
     my_x = int(input("Írj be egy számot!"))
     if my_x == 0:
         break
+    my_list.append(my_x)
 
-my_list = []
-my_list.append(my_x)
+my_list2 = my_list[::-1]
 
-print(my_list)
+print(my_list2)
+
+
 
 
