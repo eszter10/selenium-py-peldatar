bee9de2140e100aabda0fe03fc9a9384fa137e75
Laár Eszter <laar.eszter@gmail.com>
2021-07-04 19:54:03 +0200

    008 feladat kész

---
+++ selenium-homework/seleniumex.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+#from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException
+
+# In order for ChromeDriverManager to work you must pip install it in your own environment.
+#driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome()
+
+
+try:
+    driver.get("https://google.com")
+    links = driver.find_elements_by_id("no_such_element")
+except NoSuchElementException as e:
+    print('Element not found: ', e)
+finally:
+    driver.close()
