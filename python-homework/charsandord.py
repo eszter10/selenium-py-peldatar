e4e9f3fb53d63872034cf4266f5f68f100c3614b
Laár Eszter <laar.eszter@gmail.com>
2021-07-04 19:46:45 +0200

    022 feladat kész

--- python-homework/charsandord.py
+++ python-homework/charsandord.py
@@ -1,9 +1,16 @@
 #kódok és a hozzátartozó betűk kiiratása
-for i in range(26):
-    szam = i + 97
-    print(szam, chr(szam))
+#for i in range(26):
+#    szam = i + 97
+#    print(szam, chr(szam))
 
 #sorokba rendezés
 #print(szam, chr(szam,),szam, chr(szam,),szam, chr(szam,))
-
-
+abc = ('abcdefghijklmnopqrstuvwxyz')
+sor = ''
+for i in range (0,len(abc)):
+    betu = abc [i]
+    if i > 0 and (i % 5) == 0 :
+        print(sor)
+        sor = ''
+    sor += '\t'+betu+' '+str(ord(betu))
+print(sor)
