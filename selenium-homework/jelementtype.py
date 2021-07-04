9ff903ea9bf55156496dab363e7455813c99bb27
Laár Eszter <laar.eszter@gmail.com>
2021-07-04 18:22:22 +0200

    jelementtype

--- selenium-homework/jelementtype.py
+++ selenium-homework/jelementtype.py
@@ -1,3 +1,10 @@
+#from selenium import webdriver
+#from webdriver_manager.chrome import ChromeDriverManager
+#from selenium.common.exceptions import NoSuchElementException
+
+# In order for ChromeDriverManager to work you must pip install it in your own environment.
+#driver = webdriver.Chrome(ChromeDriverManager().install())
+
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
