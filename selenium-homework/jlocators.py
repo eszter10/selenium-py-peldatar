1ccbaeee548fc6efe6c4a756af00633eee1cde8a
Laár Eszter <laar.eszter@gmail.com>
2021-07-04 18:45:34 +0200

    jlocators

---
+++ selenium-homework/jlocators.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+
+driver = webdriver.Chrome()
+
+try:
+    driver.get("https://gentle-bay-0e4e13803.azurestaticapps.net/kitchensink.html")
+
+    driver.find_element_by_name("cars")
+    driver.find_element_by_id("bmwradio")
+    driver.find_element_by_xpath('//*[@id="openwindow"]')
+
+    print("test finished OK")
+
+except NoSuchElementException as e:
+    print('Element not found: ', e)
+finally:
+    driver.close()
+
+
