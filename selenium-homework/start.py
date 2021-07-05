f9cec71d560a101cb3864f63ac9f6adba90bd921
Laár Eszter <laar.eszter@gmail.com>
2021-07-05 15:51:07 +0200

    006 feladat kész

--- selenium-homework/start.py
+++ selenium-homework/start.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import time
 
 driver = webdriver.Chrome()
 
@@ -6,8 +7,13 @@ driver.get("https://www.google.com/")
 
 q = driver.find_element_by_name("q")
 q.send_keys("idokep.hu")
+time.sleep(5)
 
-submit = driver.find_element_by_name('btnK')
-submit.click()
+#submit = driver.find_element_by_name('btnK')
+#submit.click()
+
+q.submit()
+
+time.sleep(3)
 
 driver.close()
